51480824dc05a0beea3bf98e0ff140ee26aa6041
ricardo-graywolf <ricardo@graywolf.co>
2021-05-24 13:06:24 -0600

    Customize SFRA Controllers and Routes

---
+++ cartridges/app_custom_storefront/cartridge/controllers/Product.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var server = require('server');
+var page = module.superModule;        //inherits functionality from next Product.js found to the right on the cartridge path
+server.extend(page);                  //extends existing server object with a list of new routes from the Product.js found by module.superModule
+
+server.append('Show', function (req, res, next) { //adds additional middleware
+    var viewData = res.getViewData();
+    viewData.product.reviews = [{
+        text: 'Lorem ipsum dolor sit amet, cibo utroque ne vis, has no sumo graece.' +
+          ' Dicta persius his id. Ea maluisset scripserit contentiones quo, est ne movet dicam.' +
+          ' Equidem scriptorem vis no. Civibus tacimates interpretaris has et,' +
+          ' ei offendit ocurreret vis, eos purto pertinax eleifend ea.',
+        rating: 3.5
+    }, {
+        text: 'Very short review',
+        rating: 5
+    }, {
+        text: 'Lorem ipsum dolor sit amet, cibo utroque ne vis, has no sumo graece.',
+        rating: 1.5
+    }];
+
+    res.setViewData(viewData);
+    next();
+});
+
+module.exports = server.exports();
